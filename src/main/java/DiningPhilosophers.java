a2efa691a3ac1c1905ff2789ca7bdb7d0e422591
Eirik <eiriknd@stud.ntnu.no>
2022-04-05 09:55:51 +0200

    DiningPhilosophers class
    created simulation of the round table sequence

--- src/main/java/DiningPhilosophers.java
+++ src/main/java/DiningPhilosophers.java
@@ -1,2 +1,33 @@
 public class DiningPhilosophers {
+
+    public static Philosopher[] createPhilosopher(int n) {
+
+        Chopstick[] chopsticks = new Chopstick[n];
+
+        for (int i = 0; i < n; i++){
+            chopsticks[i] = new Chopstick(i);
+
+        }
+
+        Philosopher[] philosophers = new Philosopher[n];
+
+        for (int i = 0; i < n; i++){
+
+            Chopstick leftChopstick = chopsticks[i];
+            Chopstick rightChopstick = chopsticks[(i + 1) % n];
+
+            philosophers[i] = new Philosopher(i, leftChopstick, rightChopstick);
+        }
+
+        return philosophers;
+    }
+
+    public static void main(String[] args) {
+
+        Philosopher[] philosophers = createPhilosopher(5);
+
+        for (Philosopher philosopher : philosophers) {
+            philosopher.start();
+        }
+    }
 }
